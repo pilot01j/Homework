a395d0a623089b8fc4168045c314b0f992e2cb24
pilot01j <50747955+pilot01j@users.noreply.github.com>
2019-05-28 22:09:40 +0300

    Update LearningWhile.java

--- Lesson7-Switch+While/LearningWhile.java
+++ Lesson7-Switch+While/LearningWhile.java
@@ -1,45 +1,23 @@
 public class LearningWhile {
-
 	public static void main(String[] args) {
-
 	byte a=0;
-
 	byte b=0;// am adaugat "byte b=0" p/u ca programul facea doar prima intructiune "white (0<=100)" , de ce nu efectua pe restu nu am inteles
-      
-	System.out.println( "Numere(0-100):");
-
+      	System.out.println( "Numere(0-100):");
 	while(a<=100){
-
 	System.out.println(a);
-
 	a++;
 }
-
-
 	System.out.println("Numere Pare (0-50):");
-
 	while(b<=50){
-
 	if (b%2==0)if(b!=0)//am adaugat exceptia "b!=0" p/u ca imi arata ca 0 este numar par
-
 	System.out.println(b);
-
 	b++;
 }
-
-
 	System.out.println("Numere Impare (50-100): ");
-
 	while (b<=100){
-
 	if (b%2!=0)
-
 	System.out.println(b);
-
 	b++;
 }
-
 	}
-
-
 }
