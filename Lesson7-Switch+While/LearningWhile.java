cfdbb9a1ed33754bfbde61d86f0ad0b3e385e17d
pilot01j <50747955+pilot01j@users.noreply.github.com>
2019-05-29 00:57:41 +0300

    Update LearningWhile.java

--- Lesson7-Switch+While/LearningWhile.java
+++ Lesson7-Switch+While/LearningWhile.java
@@ -1,7 +1,7 @@
 public class LearningWhile {
 	public static void main(String[] args) {
 	byte a=0;
-	byte b=0;// am adaugat "byte b=0" p/u ca programul facea doar prima intructiune "white (0<=100)" , de ce nu efectua pe restu nu am inteles
+	byte b=0;/* am adaugat "byte b=0" p/u ca programul facea doar prima intructiune "white (0<=100)" ,de ce nu efectua pe restu nu am inteles*/
       	System.out.println( "Numere(0-100):");
 	while(a<=100){
 	System.out.println(a);
@@ -14,7 +14,8 @@ public class LearningWhile {
 	b++;
 }
 	System.out.println("Numere Impare (50-100): ");
-	while (b<=100){
+	while (b<=100){//aici puteam sa scriu (b<=100 && b>=50) ca sa-mi arate nr impare >50 , dar merge si fara &&b>=50 ,interesant!
+	//poate dupa primul while b ramine egal cu 50 ?
 	if (b%2!=0)
 	System.out.println(b);
 	b++;
