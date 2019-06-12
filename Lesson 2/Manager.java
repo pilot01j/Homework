3aa6763ae9d1285f55d3e59dd661d5084a228676
Pilot01j <pilot01j@gmail.com>
2019-06-12 18:03:04 +0300

    Change repozitory

--- Lesson 2/Manager.java
+++
@@ -1,5 +0,0 @@
-public class Manager {
-  public static void main (String[] args) {
-    System.out.println ("Hello, I am a Manager");
-}
-}
--- Lesson8-While+Do+For/forProgram.java
+++
@@ -1,8 +0,0 @@
-public class forProgram  {
-	public static void main(String[] args) {
-	int num=8;
-	for (int i=0; i<= 40; i++){
-	System.out.println (num*i);}
-	}
-}
-
