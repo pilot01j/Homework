6e14ea6d4c906415199a18234c3cf166da236c5b
Pilot01j <pilot01j@gmail.com>
2019-08-07 00:34:05 +0300

    Compare Objects

---
+++ Homework/Employee_Equals/src/Employee/Accountant.java
@@ -0,0 +1,8 @@
+package Employee;
+
+public class Accountant extends Employee{
+    
+    public Accountant(String name, String surname, String age, String idnp) {
+        super(name, surname, age, idnp);
+    }
+  }
